c7f5ed18bda4483835169fd75c3b48abfd262a31
mjens12 <mjens12@gmail.com>
2019-10-02 16:10:38 -0400

    More

--- main.c
+++ main.c
@@ -2,6 +2,11 @@
 #include "logic.h"
 
 int main(int argc, char *argv[]){
+	int width = 0;
+	if (strcmp(argv[1], "-w") == 0){
+		width = argv[2];
+	}
+	/* Testing stuff */
 	char next[10];
 	next_word(next);
 	printf("%s", next);
