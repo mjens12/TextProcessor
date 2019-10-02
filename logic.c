a355414529bd0d759325c932d45643623cf43847
mjens12 <mjens12@gmail.com>
2019-10-02 15:57:56 -0400

    Basic stuff

--- logic.c
+++ logic.c
@@ -1,3 +1,3 @@
 void next_word(char *next[]){
-	next = "next";
+	strcpy(next, "Hello");
 }
--- main.c
+++ main.c
@@ -2,8 +2,9 @@
 #include "logic.h"
 
 int main(int argc, char *argv[]){
-	char next[5];
-	next_word(&next);
-	printf("%s", &next);
+	char next[10];
+	next_word(next);
+	printf("%s", next);
+	printf("\ntest");
 	return (1);
 }
